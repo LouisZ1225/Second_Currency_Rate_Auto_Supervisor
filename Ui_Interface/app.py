ae7a52b0b08259000685238e8d8c17d0eca70589
zgz <zhanggz1225@gmail.com>
2026-04-11 02:09:59 +0800

    improve ui perfromance

--- Ui_Interface/app.py
+++ Ui_Interface/app.py
@@ -2,11 +2,17 @@
 
 import sys
 import os
+import pytz
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import streamlit as st
 from Analyse_and_Present.calculate_exchange import get_rate
-from datetime import date
+from datetime import date, datetime
+
+# ===== 配置模块 ======
+
+china_tz = pytz.timezone("Asia/Shanghai")
+china_today = datetime.now(china_tz).date()
 
 # ===== 界面模块 ======
 
@@ -14,7 +20,7 @@ st.title("💱 Louis的汇率查询助手")
 
 # ===== 输入模块 ======
 
-date = st.date_input("选择日期", value = date.today())
+date = st.date_input("选择日期", value = china_today)
 data = str(date)
 base = st.selectbox("基准货币", ["USD", "EUR", "GBP", "CNY", "JPY"])
 target = st.selectbox("目标货币", ["CNY", "USD", "EUR", "GBP", "JPY"])
@@ -22,9 +28,13 @@ target = st.selectbox("目标货币", ["CNY", "USD", "EUR", "GBP", "JPY"])
 # ===== 交互模块 ======
 
 if st.button("查询"):
-    rate = get_rate(date, base, target)
-
-    if rate is not None:
-        st.success(f"{base} 兑换 {target} 汇率是 {rate:.4f}")
+    
+    rate = get_rate(data, base, target)
+    
+    if date == china_today and rate is None:
+        st.warning("⚠️ 今日汇率数据尚未更新，请于每日8:30am再试")
+    
+    elif rate is not None:
+            st.success(f"{base} 兑换 {target} 汇率是 {rate:.4f}")
     else:
-        st.error("查询失败，请检查数据")
+        pass
