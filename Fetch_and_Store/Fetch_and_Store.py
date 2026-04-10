524a769e1fe14274e131893844bd1ebc6a2426e6
zgz <zhanggz1225@gmail.com>
2026-04-11 00:36:00 +0800

    update to include the function of API_KEY Secret

--- Fetch_and_Store/Fetch_and_Store.py
+++ Fetch_and_Store/Fetch_and_Store.py
@@ -13,10 +13,13 @@ def load_config():
 
     BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-    env_path = os.path.join(BASE_DIR, "Config",".env")
-    load_dotenv(env_path)
-
     API_KEY = os.getenv("API_KEY")
+
+    if not API_KEY:
+        env_path = os.path.join(BASE_DIR, "Config",".env")
+        load_dotenv(env_path)
+        API_KEY = os.getenv("API_KEY")
+    
     if not API_KEY:
         raise ValueError("❌ API_KEY 未找到，请检查 Config/.env 文件")
 
