0d15f385930e7df27b331a9e20f09ee152c6e4a3
LouisZ1225 <zhanggz1225@gmail.com>
2026-04-15 20:59:41 +0800

    insert the modules query and calculate_exchange into the project

--- Fetch_and_Store/project/Database/query.py
+++ Fetch_and_Store/project/Database/query.py
@@ -1,6 +1,24 @@
 import sqlite3
 import pandas as pd
 
+def query_rate(DB_PATH, date, currency):
+    with sqlite3.connect(DB_PATH) as conn:
+      cursor = conn.cursor()
+      cursor.execute("""
+                   SELECT rate FROM FX_RATES
+                     WHERE date = ? AND targetC = ?
+                     """, (date, currency))
+    
+      result = cursor.fetchone()
+    
+      if not result:
+            print(f"No data found for date: {date} and currency: {currency}")
+            return None
+    
+    return result[0] 
+
+
+
 def test_data(DB_PATH):
     with sqlite3.connect(DB_PATH) as conn:
         test_result = pd.read_sql_query(
